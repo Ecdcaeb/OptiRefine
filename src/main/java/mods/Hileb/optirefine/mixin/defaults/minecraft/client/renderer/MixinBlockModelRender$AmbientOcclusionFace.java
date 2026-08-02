e797ae4ecd17733c5af747c3a30776d277e27b5a
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2026-08-03 05:08:06 +0800

    fix import

--- src/main/java/mods/Hileb/optirefine/mixin/defaults/minecraft/client/renderer/MixinBlockModelRender$AmbientOcclusionFace.java
+++ src/main/java/mods/Hileb/optirefine/mixin/defaults/minecraft/client/renderer/MixinBlockModelRender$AmbientOcclusionFace.java
@@ -8,6 +8,7 @@ import net.minecraft.util.math.BlockPos;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
